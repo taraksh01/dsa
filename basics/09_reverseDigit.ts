d4f44afae3e6dbb19419c86d67d674ffc743f546
tarak <135436617+taraksh01@users.noreply.github.com>
2024-08-05 12:42:03 +0530

    basics/09_reverseDigit.ts

---
+++ basics/09_reverseDigit.ts
@@ -0,0 +1,19 @@
+import { createInterface } from "readline";
+
+const rl = createInterface({ input: process.stdin, output: process.stdout });
+rl.question(`Enter a number: `, (input: string) => {
+  const num: number = Number(input);
+  process.stdout.write(`The reverse of ${num} is ${reverseDigit(num)}\n`);
+  rl.close();
+});
+
+// Time complexity => O(N)
+// Space complexity => O(1)
+function reverseDigit(input: number): number {
+  let reverse: number = 0;
+  while (input > 0) {
+    reverse = reverse * 10 + (input % 10);
+    input = Math.floor(input / 10);
+  }
+  return reverse;
+}
